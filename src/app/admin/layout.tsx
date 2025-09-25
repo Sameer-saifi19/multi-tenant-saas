89e48f2abfd6449f4f855c3513b4f556754359a9
Sameer Saifi <Sameer.edu19@gmail.com>
2025-09-25 14:46:50 +0530

    added sidebar and pages

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,22 @@
+import AppSidebar from "@/components/global/appSidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import React from "react";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const DashboardLayout = ({ children }: Props) => {
+  return (
+    <>
+      <SidebarProvider>
+        <AppSidebar/>
+        <main className="w-full">
+          <div className="p-4">{children}</div>
+        </main>
+      </SidebarProvider>
+    </>
+  );
+};
+
+export default DashboardLayout;
---
+++ src/components/global/appSidebar/index.tsx
@@ -0,0 +1,116 @@
+'use client'
+
+import {
+  User2,
+  ChevronUp,
+  SearchIcon,
+  LayoutDashboard,
+  UsersRound,
+  CircleDollarSign,
+} from "lucide-react";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuBadge,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarSeparator,
+} from "@/components/ui/sidebar";
+import Link from "next/link";
+import Image from "next/image";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+
+const items = [
+  {
+    title: "Dashboard",
+    url: "/admin/dashboard",
+    icon: LayoutDashboard,
+  },
+  {
+    title: "Members",
+    url: "/admin/members",
+    icon: UsersRound,
+  },
+  {
+    title: "Payments",
+    url: "/admin/payments",
+    icon: CircleDollarSign,
+  },
+];
+
+const AppSidebar = () => {
+  return (
+    <Sidebar collapsible="icon">
+      <SidebarHeader className="py-4">
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton asChild>
+              <Link href="/">
+                <Image src="/globe.svg" width={20} height={20} alt="logo"></Image>
+                <span className="text-2xl font-bold uppercase tracking-wide">SAAS</span>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarSeparator/>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>gym name</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                  {item.title === "Inbox" && (
+                    <SidebarMenuBadge>24</SidebarMenuBadge>
+                  )}
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+
+      {/* SIDEBAR FOOTER */}
+
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton>
+                  <User2 /> <ChevronUp className="ml-auto" />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuItem>Account</DropdownMenuItem>
+                <DropdownMenuItem>Setting</DropdownMenuItem>
+                <DropdownMenuItem>Sign out</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
+  );
+};
+
+export default AppSidebar;
