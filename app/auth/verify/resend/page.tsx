52b1aeb01a464cc1a2daa99ed95faf3754d33779
Sameer Saifi <Sameer.edu19@gmail.com>
2025-12-15 21:21:41 +0530

    final v2

--- app/auth/verify/resend/page.tsx
+++ app/auth/verify/resend/page.tsx
@@ -1,12 +1,11 @@
 import SendVerificationEmailForm from "@/components/global/verify-email-form";
-import { redirect } from "next/navigation";
 
 interface PageProps {
   searchParams: Promise<{ error: string }>;
 }
 
 export default async function Page({ searchParams }: PageProps) {
-  const error = (await searchParams).error;
+  const token = (await searchParams);
 
   return <SendVerificationEmailForm />;
 }
--- lib/auth.ts
+++ lib/auth.ts
@@ -55,6 +55,12 @@ export const auth = betterAuth({
       });
     },
   },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+    }
+  },
   advanced: {
     database: {
       generateId: false,
@@ -85,7 +91,7 @@ export const auth = betterAuth({
   },
   account: {
     accountLinking: {
-      enabled: false,
+      enabled: true,
     },
   },
 
