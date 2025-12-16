9e6460368e6c12499c1c0047c62b376b8d9c56c5
Sameer Saifi <Sameer.edu19@gmail.com>
2025-12-16 10:19:52 +0530

    removed dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import { ThemeProvider } from "@/components/theme/theme-provider";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -29,15 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
+       
           {children}
           <Toaster richColors position="bottom-right" />
-        </ThemeProvider>
       </body>
     </html>
   );
