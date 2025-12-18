e77f743e787981963a3e1ed8ca1fbe42c9eafa65
Sameer Saifi <Sameer.edu19@gmail.com>
2025-12-18 20:09:01 +0530

    different app

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -3,18 +3,8 @@ import { createAuthClient } from "better-auth/react";
 import { auth } from "./auth";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: process.env.EXPRESS_PUBLIC_API_URL,
   plugins: [inferAdditionalFields<typeof auth>()],
 });
 
-export const {
-  signUp,
-  signIn,
-  signOut,
-  getSession,
-  useSession,
-  resetPassword,
-  sendVerificationEmail,
-  requestPasswordReset,
-  verifyEmail
-} = authClient;
+export const { signUp, signIn, signOut, getSession, useSession } = authClient;
